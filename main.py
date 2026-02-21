d5594fac2ac7bda57722e650e22ca39e27d1f555
Mohammed Musab <TheStoryLine69@gmail.com>
2026-02-21 21:25:44 +0400

    Refactor main.py for file handling and validation
    Refactored main.py to include file path handling and check for existing files before cleaning.

--- main.py
+++ main.py
@@ -1,54 +1,43 @@
-test = "test"
-
-# Run this how file run
+# Libraries
 from time import sleep as s
 from colorama import Fore as F, init
 from process import processing
 from count import count
+from data_cleaing import clean
+from pathlib import Path
+
 # Valiables
-
-delete_data = False # Defualt False
-show = False        # Defualt False
-
-invaild = F.RED + "Error: Invaild Input... System will countinue in Defualt!"
-
-init(autoreset=True)
-
-F.RESET
+delete_data = False                                                                 # Defualt False
+show = False                                                                        # Defualt False
+invaild = F.RED + "Error: Invaild Input... System will countinue in Defualt!"       # Error message
+init(autoreset=True)                                                                # Reset color
 
 # Welcome
 print("Lazy Data Cleaner")
-s(1)
+s(0.25)
+
+# File path
+base_directory = Path(__file__).resolve().parent
+data_directory = base_directory / "Data"
+data_csv = list(data_directory.glob("*.csv"))
 
 # Run all commands in process.py
 processing()
 
-# Respone for deleting data
-delete_respone = input(F.YELLOW +"Can the system delete columns/rows, recommended for large datasets (Y/N)?").lower()
+# Check if there is files before start cleaning data
+files_exist = processing()
 
-if delete_respone == "y":
-    delete_data = True
-    print(F.GREEN + "System can now delete data.")
-elif delete_respone == "n":
-    print(F.GREEN + "System won't delete data")
+if not files_exist:
+    
+    # Exit if there is no files to clean
+    print(F.RED + "No files to clean. Exiting.")
+    s(0.5)
+
 else:
-    print(F.RED + invaild)
-s(1)
-
-# Show tables
-show_table = input(F.YELLOW + "Do you want to see the tables? (Y/N)?").lower()
-if show_table == "y":
-    show = True
-    print(F.GREEN + "System will show tables.")
-elif show_table == "n":
-    print(F.GREEN + "System will not show tables")
-else:
-    print(F.RED + invaild)
-s(1)
-
-# Run all commands in count.py
-count(show, delete_data)
-s(1)
+    
+    # Run all commands in count.py
+    count(data_csv)
+    s(1)
 
 # End
 print(F.GREEN + "Lazy Data Cleaner have finished all cleaning!")
