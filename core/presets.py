baa7a1c5084fec39de71b58cda66ce8a0f4b44a3
Mohammed Musab <TheStoryLine69@gmail.com>
2026-03-28 17:18:54 +0400

    Update presets.py

--- core/presets.py
+++ core/presets.py
@@ -7,7 +7,6 @@ def default(data_csv):
 
 # AI preset
 def AI(data_csv):
-
     fill_median(True, 30, data_csv)
     fill_mode(True, 30, data_csv)
     outlier(data_csv)
@@ -16,13 +15,11 @@ def AI(data_csv):
 
 # Bussiness preset
 def bussinses(data_csv):
-
     fill_mode(False, 5, data_csv)
     duplicate(data_csv)
 
 # Streaming preset
 def streaming(data_csv):
-
     fill_mode(True, 50, data_csv)
     outlier(data_csv)
     duplicate(data_csv)
